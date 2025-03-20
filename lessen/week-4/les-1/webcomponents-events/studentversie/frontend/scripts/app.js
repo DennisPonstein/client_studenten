ab7cddbecd8a20ad9490921624e15bdfe4803516
DennisPonstein <54404151+DennisPonstein@users.noreply.github.com>
2025-03-20 09:44:41 +0100

    opdrachten

--- lessen/week-4/les-1/webcomponents-events/studentversie/frontend/scripts/app.js
+++ lessen/week-4/les-1/webcomponents-events/studentversie/frontend/scripts/app.js
@@ -1,56 +1,66 @@
-import {template} from "./modules/template.js";
-import {Cirkel} from "./components/cirkel.js";
-import {Vierkant} from "./components/vierkant.js";
+import { template } from "./modules/template.js";
+import { Cirkel } from "./components/cirkel.js";
+import { Vierkant } from "./components/vierkant.js";
+import { Driehoek } from "./components/driehoek.js";
 
 class App extends HTMLElement {
+  shadowRoot;
+  templateId = "app-click-cirkel-tpl";
+  elementId = "app-click-cirkel";
 
-    shadowRoot;
-    templateId = 'app-click-cirkel-tpl';
-    elementId = 'app-click-cirkel';
+  constructor() {
+    super(); // always call super() first in the ctor.
+    this.shadowRoot = this.attachShadow({ mode: "open" });
+    this.init();
+  }
 
-    constructor() {
-        super(); // always call super() first in the ctor.
-        this.shadowRoot = this.attachShadow({mode: 'open'})
-        this.init();
-    }
+  init() {
+    //plaats alle HTML templates in de DOM
+    template.attachTemplates();
+  }
 
-    init() {
-        //plaats alle HTML templates in de DOM
-        template.attachTemplates();
-    }
+  connectedCallback() {
+    this.applyTemplate();
+    this.attachStyling();
+    this.applyEventlisteners();
 
-    connectedCallback() {
-        this.applyTemplate();
-        this.attachStyling();
-        this.applyEventlisteners();
+    console.log(`De huidige versie van de applicatie is: 
+        ${this.getAttribute("versie")}`);
+  }
 
-        console.log(`De huidige versie van de applicatie is: 
-        ${this.getAttribute('versie')}`)
-    }
+  applyTemplate() {
+    let appTemplate = document.getElementById(this.templateId);
+    let clone = appTemplate.content.cloneNode(true);
+    this.shadowRoot.appendChild(clone);
+  }
 
-    applyTemplate() {
-        let appTemplate = document.getElementById(this.templateId);
-        let clone = appTemplate.content.cloneNode(true);
-        this.shadowRoot.appendChild(clone);
-    }
+  // app.js
+  attachStyling() {
+    const linkElem = document.createElement("link");
+    linkElem.setAttribute("rel", "stylesheet");
+    linkElem.setAttribute("href", "stylesheets/app.css");
+    this.shadowRoot.appendChild(linkElem);
+  }
 
-    // app.js
-    attachStyling(){
-        const linkElem = document.createElement("link");
-        linkElem.setAttribute("rel", "stylesheet");
-        linkElem.setAttribute("href", "stylesheets/app.css");
-        this.shadowRoot.appendChild(linkElem);
-    }
+  applyEventlisteners() {
+    this.addEventListener("cirkelClick", this.cirkelClickedHandler);
+  }
 
-    applyEventlisteners(){
-        this.addEventListener('cirkelClick', this.cirkelClickedHandler);
-    }
+  cirkelClickedHandler(event) {
+    let cirkelComponent = this.shadowRoot.getElementById("cirkel-1");
+    cirkelComponent.setState(
+      "cirkelClicks",
+      ++cirkelComponent.state.cirkelClicks
+    );
+  }
 
-    cirkelClickedHandler(event) {
-        let cirkelComponent = this.shadowRoot.getElementById('cirkel-1');
-        cirkelComponent.setState('cirkelClicks', ++cirkelComponent.state.cirkelClicks);
-    }
-
+  driehoekClickedHandler(event) {
+    let cirkelComponent = this.shadowRoot.getElementById("driehoek-1");
+    cirkelComponent.setState(
+      "driehoekClicks",
+      ++cirkelComponent.state.cirkelClicks
+    );
+  }
 }
 
-customElements.define('app-click-cirkel', App);
+customElements.define("app-click-cirkel", App);
--- lessen/week-4/les-1/webcomponents-events/studentversie/frontend/scripts/modules/template.js
+++ lessen/week-4/les-1/webcomponents-events/studentversie/frontend/scripts/modules/template.js
@@ -1,54 +1,62 @@
-
 const appTemplate = {
-    id: 'app-click-cirkel-tpl',
-    template: `
+  id: "app-click-cirkel-tpl",
+  template: `
     <div id="app-click-cirkel" class="app">
     <h1>Click de cirkel</h1>
         <div class="cirkels">
             <click-cirkel id="cirkel-1" color="red">a</click-cirkel>
             <click-cirkel id="cirkel-2" color="blue">b</click-cirkel>
             <click-vierkant id="vierkant-1" color="blue">default value - geen webcomponent</click-vierkant>
+            <click-driehoek id="driehoek-1" color="red"></driehoek>
         </div>
     </div> 
-    `
-}
+    `,
+};
 
 const cirkelTemplate = {
-    id: 'click-cirkel-tpl',
-    template: `
+  id: "click-cirkel-tpl",
+  template: `
     <div class="cirkel">
         <p data-bind="cirkelClicks"></p>
     </div> 
-    `
-}
+    `,
+};
 
 const vierkantTemplate = {
-    id: 'click-vierkant-tpl',
-    template: `
+  id: "click-vierkant-tpl",
+  template: `
     <div class="vierkant">
         <p data-bind="vierkantClicks">vierkant</p>
     </div> 
-    `
-}
+    `,
+};
 
-class Template{
+const driehoekTemplate = {
+  id: "click-driehoek-tpl",
+  template: `
+    <div class="driehoek">
+        <p data-bind="driehoekClicks"></p>
+    </div> 
+    `,
+};
 
-    attachTemplates(){
-        this.attachTemplate(appTemplate);
-        this.attachTemplate(cirkelTemplate);
-        this.attachTemplate(vierkantTemplate);
-    }
+class Template {
+  attachTemplates() {
+    this.attachTemplate(appTemplate);
+    this.attachTemplate(cirkelTemplate);
+    this.attachTemplate(vierkantTemplate);
+    this.attachTemplate(driehoekTemplate);
+  }
 
-    attachTemplate(tplObject){
-        const templateNode = document.createElement('template');
-        templateNode.id = tplObject.id;
-        templateNode.innerHTML = tplObject.template;
-        const body = document.querySelector('body');
-        body.appendChild(templateNode);
-    }
-
+  attachTemplate(tplObject) {
+    const templateNode = document.createElement("template");
+    templateNode.id = tplObject.id;
+    templateNode.innerHTML = tplObject.template;
+    const body = document.querySelector("body");
+    body.appendChild(templateNode);
+  }
 }
 
 const template = new Template();
 
-export {template};
+export { template };
